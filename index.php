a0029229932fa7afb4b1dea60d46323476b28bce
Lloyd <manchesterboy@gmail.com>
2012-09-10 11:34:57 +0200

    commented usage code

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 set_include_path(dirname(__FILE__) . '/library:'. get_include_path());
-set_include_path(dirname(__FILE__) . '/tests:'. get_include_path());
 
 require_once('Responder.php');
 require_once('Configuration.php');
@@ -20,9 +19,7 @@ $db   = $db_config['dbname'];
 $db = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$log_file = 'live.log';
-$responder = new Responder(1, $config, $db, $log_file);
-
+// USAGE
+$responder = new Responder(1, $config, $db);
 $xml = $_POST['xml'];
-
 $responder->handleResponse($xml);
